6a4698db9944567118951767a4acbada14c89b3f
piyush <pmimpiyush@gmail.com>
2024-06-07 15:12:27 +0530

    .

--- routes/getChats.js
+++ routes/getChats.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser')
+const team = require("../schema/team");
+
 const chatdb = require("../schema/chat");
-router.post("/", async (req, res) => {
-  const { teamname } = req.body;
+
+
+router.get("/", fetchuser, async (req, res) => {
+  const user = userfromtoken.id;
+  const information = await team.findOne({_userid:user});
+if(!information){
+res.status(404).send("cant find info")
+}
+ 
+  const  teamname = information.teamname; 
   try {
     const chatinfo = await chatdb.find({ group: teamname });
 
@@ -10,7 +21,7 @@ router.post("/", async (req, res) => {
       res.status(404).send("cant find chats");
     }
 
-    return res.status(200).send(chatinfo);
+    return res.status(200).send({chatinfo:chatinfo,information:information});
   } catch (error) {
     res.send(" something error ");
   }
